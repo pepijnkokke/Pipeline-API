fa264f3278895be5cf5ad549c24db64005cb423a
Wen Kokke <wenkokke@users.noreply.github.com>
2014-05-23 19:48:53 +0200

    added first and second projection functions

--- util/src/main/java/semante/pipeline/impl/IPair.java
+++ util/src/main/java/semante/pipeline/impl/IPair.java
@@ -1,10 +1,30 @@
 package semante.pipeline.impl;
 
+import com.google.common.base.Function;
+
 import semante.pipeline.Pair;
 import lombok.experimental.Value;
 
 @Value
 public final class IPair<F, S> implements Pair<F, S> {
+	
+	public static final <F,S> Function<Pair<F,S>,F> first() {
+		return new Function<Pair<F,S>,F>() {
+			@Override
+			public final F apply(Pair<F, S> p) {
+				return p.getFirst();
+			}
+		};
+	}
+	
+	public static final <F,S> Function<Pair<F,S>,S> second() {
+		return new Function<Pair<F,S>,S>() {
+			@Override
+			public final S apply(Pair<F, S> p) {
+				return p.getSecond();
+			}
+		};
+	}
 	
 	public static final <F,S> Pair<F,S> pair(F fst, S snd) {
 		return new IPair<F,S>(fst, snd);
