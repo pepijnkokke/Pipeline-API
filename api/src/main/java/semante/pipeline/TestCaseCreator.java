f1e430324042e45682b4354f6e4f8dd1f2e38e53
Wen Kokke <wenkokke@users.noreply.github.com>
2014-06-09 12:25:01 +0200

    Removed methods, amending.

--- api/src/main/java/semante/pipeline/TestCaseCreator.java
+++ api/src/main/java/semante/pipeline/TestCaseCreator.java
@@ -33,33 +33,4 @@ public interface TestCaseCreator {
 			final SimpleBinaryTree<Annotation> hypothesis,
 			final Iterable<Pair<SimpleBinaryTree<Annotation>, SimpleBinaryTree<Annotation>>> subs,
 			final ResultType resultType);
-	
-	/**
-	 * Creats JUnit unit tests based upon the current annotation.
-	 * 
-	 * @param comment the comment appears before any code
-	 * @param text the text is printed to a tree
-	 * @param hypothesis the hypothesis is printed to a tree
-	 * @param subsumptions newline separated subsumption relations
-	 * @return Java source for a JUnit unit test
-	 */
-	<ID> String createTestCase(
-			final String name,
-			final String comment,
-			final BinaryTree<ID, Annotation> text,
-			final BinaryTree<ID, Annotation> hypo,
-			final Iterable<Pair<BinaryTree<ID, Annotation>, BinaryTree<ID, Annotation>>> subs);
-	
-	/**
-	 * As {@link TestCaseCreator#createTestCase(String, String, BinaryTree, BinaryTree, String)},
-	 * but labels the nodes in the annotation trees internally.
-	 * 
-	 * <em>Note: nodes are labelled in a breadth-first maner.</em>
-	 */
-	String createTestCase(
-			final String name,
-			final String comment,
-			final SimpleBinaryTree<Annotation> text,
-			final SimpleBinaryTree<Annotation> hypothesis,
-			final Iterable<Pair<SimpleBinaryTree<Annotation>, SimpleBinaryTree<Annotation>>> subs);
 }
